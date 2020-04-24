cbcdb28164a809ad12755421c1ecc33a8172b88e
hensden <karnawat.rohan18@gmail.com>
2020-04-23 20:31:58 -0700

    Audio part added, bugs fixed

--- vectorize.py
+++ vectorize.py
@@ -9,32 +9,34 @@ from math import floor
 from PIL import Image as IM
 
 W, H = 352, 288
-folder = sys.argv[1]
-images = []
+loffolder = ['data/video_1/', 'data/video_2/','data/video_3/', 'data/video_4/']
 
-for file in tqdm(os.listdir(folder)):
-	with open('data/video_1/image-0003.rgb', 'rb') as f:
-		byte = f.read()
-	bl,gr,re = [],[],[]
-	image = np.zeros((H,W,3))
-	for i, b in enumerate(byte):
-		if i<W*H:
-			re.append(b)
-		elif i>=W*H and i<W*H*2:
-			gr.append(b)
-		else:
-			bl.append(b)
-		pass
-	image[:,:,0] = np.array(re).reshape((H,W))
-	image[:,:,1] = np.array(gr).reshape((H,W))
-	image[:,:,2] = np.array(bl).reshape((H,W))
-	image = image.astype(np.uint8)
-	images.append(image)
+for folder in loffolder:
+	print("Parsing", folder)
+	images = []
+	for file in tqdm(os.listdir(folder)):
+		with open(folder+file, 'rb') as f:
+			byte = f.read()
+		bl,gr,re = [],[],[]
+		image = np.zeros((H,W,3))
+		for i, b in enumerate(byte):
+			if i<W*H:
+				re.append(b)
+			elif i>=W*H and i<W*H*2:
+				gr.append(b)
+			else:
+				bl.append(b)
+			pass
+		image[:,:,0] = np.array(re).reshape((H,W))
+		image[:,:,1] = np.array(gr).reshape((H,W))
+		image[:,:,2] = np.array(bl).reshape((H,W))
+		image = image.astype(np.uint8)
+		images.append(image)
 	
 
-out = open(folder[:-1]+'_parsed.pkl', 'wb')
-pk.dump(images, out)
-out.close()
+	out = open(folder[:-1]+'_parsed.pkl', 'wb')
+	pk.dump(images, out)
+	out.close()
 
 print("Done!")
 
